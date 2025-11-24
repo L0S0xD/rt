793a95473010fc9ac53e67dd5dfdcd343cb8aabf
danistem <dannniellyyy@gmail.com>
2025-11-23 22:37:56 -0700

    not inserting into ratings table

--- main.py
+++ main.py
@@ -57,10 +57,10 @@ def get_rating_by_id(rating_id):
     return row
 
 
-def add_rating(df_rating: int, df_reason: str):
+def add_rating(rating_type: str, rating_name: str, lyrics_rating: int, lyrics_reason: str, beat_rating: int, beat_reason: str, df_rating: int, df_reason: str, melody_rating: int, melody_reason: str, cohesive_rating: int, cohesive_reason: str,  ):
     conn = get_db_connection()
     cur = conn.cursor()
-    cur.execute("INSERT INTO ratings (df_rating, df_reason) VALUES (?,?)", (df_reason,df_rating, ))
+    cur.execute("INSERT INTO ratings (rating_type, rating_name, lyrics_rating,lyrics_reason, beat_rating, beat_reason, df_rating, df_reason, melody_rating, melody_reason, cohesive_rating, cohesive_reason) VALUES (?,?,?,?,?,?,?,?,?,?,?,?)", (rating_type, rating_name,lyrics_rating,lyrics_reason, beat_rating, beat_reason, df_rating, df_reason, melody_rating, melody_reason, cohesive_rating, cohesive_reason))
     conn.commit()
     conn.close()
 
@@ -97,10 +97,20 @@ def home():
 @app.route("/add", methods=["GET", "POST"])
 def add():
     if request.method == "POST":
-        df_rating = request.form.get("flow", "").strip()
-        df_reason = request.form.get("flow_reason", "").strip()
-        if df_rating:
-            add_rating(df_reason, df_rating)
+        rating_type = request.form.get("rating_type", "").strip()
+        rating_name = request.form.get("rating_name", "").strip()
+        lyrics_rating = request.form.get("lyrics", "").strip()
+        lyrics_reason = request.form.get("lyrics_reason", "").strip()
+        beat_rating = request.form.get("beat", "").strip()
+        beat_reason = request.form.get("beat_reason", "").strip()
+        df_rating = request.form.get("df", "").strip()
+        df_reason = request.form.get("df_reason", "").strip()
+        melody_rating = request.form.get("melody", "").strip()
+        melody_reason = request.form.get("melody_reason", "").strip()
+        cohesive_rating = request.form.get("cohesive", "").strip()
+        cohesive_reason = request.form.get("cohesive_reason", "").strip()
+        if rating_type:
+            add_rating(rating_type, rating_name,lyrics_rating,lyrics_reason, beat_rating, beat_reason, df_rating, df_reason, melody_rating, melody_reason, cohesive_rating, cohesive_reason)
         return redirect(url_for("home"))
     return render_template("add.html")
 
