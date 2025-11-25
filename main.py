eb4180fef0fe6fe7e11f3653761d1ea6adfabd60
danistem <dannniellyyy@gmail.com>
2025-11-24 21:14:04 -0700

    SAME PROBLEMS

--- main.py
+++ main.py
@@ -28,9 +28,27 @@ def init_db():
     cur = conn.cursor()
     cur.execute(
         """
-        CREATE TABLE IF NOT EXISTS ratings (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            df_reason TEXT NOT NULL
+       CREATE TABLE IF NOT EXISTS "ratings" (
+        id INTEGER,
+        content VARCHAR(50),
+        rating_key PRIMARY KEY,
+        rating_type VARCHAR(50),
+        rating_name VARCHAR(50), 
+        lyrics_rating INTEGER,
+        lyrics_reason  VARCHAR(500),
+        beat_rating INTEGER,
+        beat_reason  VARCHAR(500),
+        df_rating INTEGER,
+        df_reason VARCHAR(500),
+        melody_rating INTEGER,
+        melody_reason  VARCHAR(500),
+        cohesive_rating INTEGER,
+        cohesive_reason  VARCHAR(500),
+        user VARCHAR(50),
+        upvotes  INTEGER,
+        downvotes INTEGER,
+        challenged INTEGER,
+        challenge_key INTEGER
         )
         """
     )
@@ -57,7 +75,7 @@ def get_rating_by_id(rating_id):
     return row
 
 
-def add_rating(rating_type: str, rating_name: str, lyrics_rating: int, lyrics_reason: str, beat_rating: int, beat_reason: str, df_rating: int, df_reason: str, melody_rating: int, melody_reason: str, cohesive_rating: int, cohesive_reason: str,  ):
+def add_rating(rating_type: str, rating_name: str, lyrics_rating: int, lyrics_reason: str, beat_rating: int, beat_reason: str, df_rating: int, df_reason: str, melody_rating: int, melody_reason: str, cohesive_rating: int, cohesive_reason: str):
     conn = get_db_connection()
     cur = conn.cursor()
     cur.execute("INSERT INTO ratings (rating_type, rating_name, lyrics_rating,lyrics_reason, beat_rating, beat_reason, df_rating, df_reason, melody_rating, melody_reason, cohesive_rating, cohesive_reason) VALUES (?,?,?,?,?,?,?,?,?,?,?,?)", (rating_type, rating_name,lyrics_rating,lyrics_reason, beat_rating, beat_reason, df_rating, df_reason, melody_rating, melody_reason, cohesive_rating, cohesive_reason))
